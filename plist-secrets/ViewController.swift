41174c8e0636f37446cf4a2e4285541671ba322e
Brian Sharon <brian@floatplane.us>
2018-01-02 14:51:01 -0800

    Add a reference to secrets.plist in the project Add code to ViewController init to load secret keys

--- plist-secrets/ViewController.swift
+++ plist-secrets/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var secret1: UILabel!
+    @IBOutlet weak var secret2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Load the dictionary of secret keys from secrets.plist
+        let path = Bundle.main.path(forResource: "secrets", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        // Set the secret key values on the UILabels
+        secret1.text = dict!.object(forKey: "API_SECRET_ONE") as? String
+        secret2.text = dict!.object(forKey: "API_SECRET_TWO") as? String
     }
 
     override func didReceiveMemoryWarning() {
